948e01e96f435330b4b0ec5893fce64ef2b8554a
Rahul Lodhi <rahullodhimts@gmail.com>
2025-06-20 22:10:53 +0530

    first commit

---
+++ src/main/java/com/tcs/training/XyzBankWithSpringbootWithJpaAndRestApiAndJUnitApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzBankWithSpringbootWithJpaAndRestApiAndJUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzBankWithSpringbootWithJpaAndRestApiAndJUnitApplication.class, args);
+	}
+
+}
