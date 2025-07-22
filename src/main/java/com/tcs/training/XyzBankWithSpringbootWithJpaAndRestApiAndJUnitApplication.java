b93a61e3204dea344e0616f85a537ccdb079e9c9
Rahul Lodhi <rahullodhimts@gmail.com>
2025-07-22 18:08:34 +0530

    replace @SpringBootApplication with 3 other annotations

--- src/main/java/com/tcs/training/XyzBankWithSpringbootWithJpaAndRestApiAndJUnitApplication.java
+++ src/main/java/com/tcs/training/XyzBankWithSpringbootWithJpaAndRestApiAndJUnitApplication.java
@@ -1,9 +1,15 @@
 package com.tcs.training;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+@Configuration
 public class XyzBankWithSpringbootWithJpaAndRestApiAndJUnitApplication {
 
 	public static void main(String[] args) {
